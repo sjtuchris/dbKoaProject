ffb15d0c1f88e3439c4baef711024c46f4c282d5
Tingjun Zhang <tz976@nyu.edu>
2017-05-05 22:58:55 -0400

    Merged+1.

--- server/models/message.js
+++ server/models/message.js
@@ -3,7 +3,7 @@ const db = require('../config/db.js'),
 const testDb = db.connectDB; // 引入数据库
 const Message = testDb.import(messageModel); // 用sequelize的import方法引入表结构，实例化了User。
 
-const getUserMessage = async function(uid) {
+const getUserSentMessage = async function(uid) {
 	const userMessages = await Message.findAll({
 		where : {
 			uid: uid
